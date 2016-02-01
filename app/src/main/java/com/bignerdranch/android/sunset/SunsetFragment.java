bc8eab2587889fbaf31d89887a4749df6bb11963
iceguyvn <nguyen_my@yahoo.com>
2016-02-01 15:08:26 -0800

    animated the sky color from Blue to Sunset as the sun goes down to the ocean; the colors kaleidoscope widly

--- app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -1,11 +1,13 @@
 package com.bignerdranch.android.sunset;
 
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 /**
  * Created by My on 2/1/2016.
@@ -14,6 +16,9 @@ public class SunsetFragment extends Fragment {
    private View   mSceneView;
    private View   mSunView;
    private View   mSkyView;
+   private int    mBlueSkyColor;
+   private int    mSunsetSkyColor;
+   private int    mNightSkyColor;
 
    public static SunsetFragment newInstance() {
       return new SunsetFragment();
@@ -25,6 +30,11 @@ public class SunsetFragment extends Fragment {
       mSceneView = view;
       mSunView = view.findViewById(R.id.sun);
       mSkyView = view.findViewById(R.id.sky);
+      // extract colors
+      Resources resources = getResources();
+      mBlueSkyColor = resources.getColor(R.color.blue_sky);
+      mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+      mNightSkyColor = resources.getColor(R.color.night_sky);
       // press anywhere on the screen to run the animation
       mSceneView.setOnClickListener(new View.OnClickListener() {
          @Override
@@ -43,7 +53,14 @@ public class SunsetFragment extends Fragment {
       float sunYEnd = mSkyView.getHeight();
       // create a property animator
       ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd).setDuration(3000);
-      // start the animator
+      // add acceleration to make the sun start out moving slowly and accelerate to a quicker pace
+      // as it moves toward the horizon.
+      heightAnimator.setInterpolator(new AccelerateInterpolator());
+      // animator for the sky from mBlueSkyColor to mSunsetSkyColor.
+      ObjectAnimator sunsetSkyAnimator = ObjectAnimator.ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor).setDuration(3000);
+      // animate the sun going down
       heightAnimator.start();
+      // animate the sky color changes
+      sunsetSkyAnimator.start();
    }
 }
