13626376fedb82bf0393ea2c8f836d4ede8de0f5
iceguyvn <nguyen_my@yahoo.com>
2016-02-01 14:56:40 -0800

    animated the sun so that it goes down to the ocean

--- app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
+++ app/src/main/java/com/bignerdranch/android/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,10 @@ import android.view.ViewGroup;
  * Created by My on 2/1/2016.
  */
 public class SunsetFragment extends Fragment {
+   private View   mSceneView;
+   private View   mSunView;
+   private View   mSkyView;
+
    public static SunsetFragment newInstance() {
       return new SunsetFragment();
    }
@@ -17,6 +22,28 @@ public class SunsetFragment extends Fragment {
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.fragment_sunset, container, false);
+      mSceneView = view;
+      mSunView = view.findViewById(R.id.sun);
+      mSkyView = view.findViewById(R.id.sky);
+      // press anywhere on the screen to run the animation
+      mSceneView.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            startAnimation();
+         }
+      });
       return view;
+   }
+
+   private void startAnimation() {
+      // animation starts with the top of the view at its current location
+      float sunYStart = mSunView.getTop();
+      // animation ends with the top at the bottom of mSunView's parent, mSkyView. to get there, it
+      // should be as far down as mSkyView is tall, by calling getHeight()
+      float sunYEnd = mSkyView.getHeight();
+      // create a property animator
+      ObjectAnimator heightAnimator = ObjectAnimator.ofFloat(mSunView, "y", sunYStart, sunYEnd).setDuration(3000);
+      // start the animator
+      heightAnimator.start();
    }
 }
